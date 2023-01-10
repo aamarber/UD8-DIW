8d78316a2fdb98fc5bd725e781cd58a4d5e2d9e3
Alvaro Maceda <alvaro@alvaromaceda.es>
2023-01-10 14:53:36 +0100

    Fragment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { Fragment } from "react"
+
+function Calendario() {
+  return (
+    <div>
+      <div id='titulo'>
+        <h1>Enero 2222</h1>
+      </div>
+      <div id='dias'>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+    </div>
+  )
+}
+
 function App() {
   return (
-    <ul id="banana">
-      <li>One</li>
-      <li>Two</li>
-      <li>Three</li>
-    </ul>
+    <Fragment>
+      <h1>Super calendario</h1>
+      <Calendario />
+    </Fragment>
   )
 }
 
 export default App
+
