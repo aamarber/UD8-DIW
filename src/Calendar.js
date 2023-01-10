687563bc9bd911bd0e13ebc26dc15ac3b54df0cb
Alvaro Maceda <alvaro@alvaromaceda.es>
2023-01-10 17:39:36 +0100

    Inline styles

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,6 +1,12 @@
 import './styles/calendar.css'
 import CalendarTitle from './CalendarTitle'
 
+const weekendStyle = {
+  backgroundColor: 'red',
+  fontWeight: 'bold',
+  fontSize: 20
+}
+
 function Calendar(props) {
   const { year, month } = props
   const holidays = (month == 7) || (month == 8)
@@ -8,7 +14,11 @@ function Calendar(props) {
   const days = [
     <div key='1'>1</div>,
     <div key='2'>2</div>,
-    <div key='3'>3</div>
+    <div key='3'>3</div>,
+    <div key='4'>4</div>,
+    <div key='5'>5</div>,
+    <div key='6' style={weekendStyle}>6</div>,
+    <div key='7' style={weekendStyle}>7</div>
   ]
 
   return (
