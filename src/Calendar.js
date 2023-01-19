e59fe5abca35342c758042ba5602a640e855927b
Alvaro Maceda <alvaro@alvaromaceda.es>
2023-01-19 14:41:04 +0100

    Adds state to calendar

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,16 +1,25 @@
+import React from 'react'
+import { useState } from 'react'
+
 import './styles/calendar.css'
 import CalendarTitle from './CalendarTitle'
 import Week from './Week'
 
 import { getWeeksInMonth } from './date_functions'
 
+
 function Calendar(props) {
   const { year, month } = props
   const holidays = (month == 7) || (month == 8)
   const weeks = getWeeksInMonth(month, year)
 
+  let [selected, setSelected] = useState(1)
+
   return (
     <div className={`calendar ${holidays ? 'holidays' : ''}`}>
+      <button onClick={() => setSelected(++selected)}>+</button>
+      <button onClick={() => setSelected(--selected)}>-</button>
+      {selected}
       <CalendarTitle month={month} year={year}/>
       {
         weeks.map((week, index) =>
