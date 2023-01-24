49988a578dde8912905d4cd88f809285055f2aa0
Alvaro Maceda <alvaro@alvaromaceda.es>
2023-01-24 16:12:01 +0100

    Adds image for conditional rendering

--- src/conditional/Conditional.js
+++ src/conditional/Conditional.js
@@ -2,6 +2,8 @@ import List from "./List"
 import Table from "./Table"
 import Text from "./Text"
 
+import banana from "./banana.png";
+
 function Conditional(props) {
 
   const data = [
@@ -10,9 +12,14 @@ function Conditional(props) {
 
   return(
     <>
+      <button>Toggle image</button>
+      <br/>
+      <img src={banana}/>
+      <br/>
       <button>List</button>
       <button>Table</button>
       <button>Text</button>
+      <br/>
       <List data={data}/>
       <Table data={data}/>
       <Text data={data}/>
